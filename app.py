293b73e723bf143c6ea3d8f8a32782d2a15195e4
Elsa Villon <elsa.villon@gmail.com>
2024-04-08 14:45:32 -0300

    Ajustes Flask 2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template
 import requests
 from bs4 import BeautifulSoup
 import unicodedata
@@ -6,11 +7,34 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+# Criar uma instância Flask
+app = Flask(__name__)
+
+# Definir rota para página inicial
+@app.route("/")
+def index():
+    return render_template("home.html")
+    
+# Definir rota para página de portfólio
+@app.route("/portfolio")
+def portifolio():
+    return render_template("portfolio.html")
+
+# Definir rota para página de currículo
+@app.route("/curriculo")
+def curriculo():
+    return render_template("curriculo.html")
+
+# Definir rota para página dinâmica com raspagem do Deutsche Welle
+@app.route("/dw")
+def dw():
+    return render_template("dw.html")
+
 # Função para remover acentos
 def remove_acentos(texto):
     return ''.join(c for c in unicodedata.normalize('NFD', texto) if unicodedata.category(c) != 'Mn')
 
-# Função para extrair manchetes e links da DW
+# Função para extrair manchetes e links da Deutsche Welle
 def manchetes_dw():
     requisicao = requests.get('https://www.dw.com/pt-br/manchetes/headlines-pt-br')
     html = requisicao.content
@@ -26,25 +50,25 @@ def manchetes_dw():
         lista_dw.append([titulo, link_sem_acentos])
     return lista_dw
 
-# Dados para conexão no servidor SMTP:
+# Dados para conexão no servidor SMTP
 smtp_server = "smtp-relay.brevo.com"
 port = 587
 email = os.environ["EMAIL_REMETENTE"]
 password = os.environ["SMTP_PASSWORD"]
 
-# Dados para o email que será enviado:
-remetente = os.environ ["EMAIL_REMETENTE"]
-destinatarios = os.environ ["EMAIL_DESTINATARIOS"]
+# Dados para o email que será enviado
+remetente = os.environ["EMAIL_REMETENTE"]
+destinatarios = os.environ["EMAIL_DESTINATARIOS"]
 
 # Extrair manchetes e links
 manchetes_links = manchetes_dw()
 
 # Construir o corpo do e-mail
-html = html = """
+html = """
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Manchetes Deutsch Welle</title>
+    <title>Manchetes Deutsche Welle</title>
   </head>
   <body>
     <h1>Destaques Semanais</h1>
@@ -54,7 +78,7 @@ html = html = """
 """
 
 # Título do email
-titulo_email = "Destaques da Semana - Deutsch Welle"
+titulo_email = "Destaques da Semana - Deutsche Welle"
 
 # Iniciar conexão com o servidor SMTP
 server = smtplib.SMTP(smtp_server, port)
@@ -72,25 +96,5 @@ mensagem.attach(conteudo_html)
 # Envio do email
 server.sendmail(remetente, destinatarios, mensagem.as_string())
 
-## Páginas do site
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template("home.html")
-    
-@app.route("/portfolio")
-def portifolio ():
-    return render_template("portfolio.html")
-
-@app.route("/curriculo")
-def curriculo ():
-    return render_template("curriculo.html")
-
-## Parte da página dinâmica com raspagem do BBC 
-
-@app.route("/dw")
-def dw ():
-    return render_template("dw.html")
-
+if __name__ == "__main__":
+    app.run()
