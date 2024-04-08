52c91ebbdde6b07243fddf1898af4d57a36c8121
Elsa Villon <elsa.villon@gmail.com>
2024-04-08 15:19:39 -0300

    Ajustes importação Flask

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import Flask
+import flask
 
 # Função para remover acentos
 def remove_acentos(texto):
