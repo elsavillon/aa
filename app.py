226465dfeeb4f92a29f35800d2b07632e5d8008e
Elsa Villon <elsa.villon@gmail.com>
2024-04-08 17:17:04 -0300

    Ajustes envio e-mail 5

--- app.py
+++ app.py
@@ -27,54 +27,59 @@ def manchetes_dw():
 
 smtp_server = "smtp-relay.brevo.com"
 port = 587
-email_remetente = os.environ.get("EMAIL_REMETENTE")
-smtp_password = os.environ.get("SMTP_PASSWORD")
-email_destinatarios = os.environ.get("EMAIL_DESTINATARIOS")
+email = os.environ["EMAIL_REMETENTE"]
+password = os.environ["SMTP_PASSWORD"]
+
+remetente = os.environ["EMAIL_REMETENTE"]
+destinatarios = os.environ["EMAIL_DESTINATARIOS"].split(',')
+
+envio_realizado = False
 
 app = Flask(__name__)
 
 def enviar_email():
-    manchetes_links = manchetes_dw()
+    global envio_realizado
+    if not envio_realizado:
+        manchetes_links = manchetes_dw()
 
-    html = """
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Manchetes Deutsche Welle</title>
-      </head>
-      <body>
-        <h1>Destaques Semanais</h1>
-        <p>
-          Sem tempo para ler as notícias? Sem problemas, eu fiz a ronda no Deutsche Welle e trago os destaques:
-          <ul>
-    """
-    for titulo, link in manchetes_links:
-        html += f'<li> <a href="{link}">{titulo}</a> </li>'
-    html += """
-          </ul>
-        </p>
-      </body>
-    </html>
-    """
+        html = """
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Manchetes Deutsche Welle</title>
+          </head>
+          <body>
+            <h1>Destaques Semanais</h1>
+            <p>
+              Sem tempo para ler as notícias? Sem problemas, eu fiz a ronda no Deutsche Welle e trago os destaques:
+              <ul>
+        """
+        for titulo, link in manchetes_links:
+            html += f'<li> <a href="{link}">{titulo}</a> </li>'
+        html += """
+              </ul>
+            </p>
+          </body>
+        </html>
+        """
 
-    titulo_email = "Destaques da Semana - Deutsche Welle"
+        titulo_email = "Destaques da Semana - Deutsche Welle"
 
-    if email_remetente and smtp_password and email_destinatarios:
         server = smtplib.SMTP(smtp_server, port)
         server.starttls()  
-        server.login(email_remetente, smtp_password)  
+        server.login(email, password)  
 
         mensagem = MIMEMultipart()
-        mensagem["From"] = email_remetente
-        mensagem["To"] = email_destinatarios
+        mensagem["From"] = remetente
+        mensagem["To"] = ",".join(destinatarios)
         mensagem["Subject"] = titulo_email
         conteudo_html = MIMEText(html, "html")  # Adiciona a versão em HTML
         mensagem.attach(conteudo_html)
 
-        server.sendmail(email_remetente, email_destinatarios.split(','), mensagem.as_string())
+        server.sendmail(remetente, destinatarios, mensagem.as_string())
         server.quit()
-    else:
-        print("Erro: Verifique as variáveis de ambiente EMAIL_REMETENTE, SMTP_PASSWORD e EMAIL_DESTINATARIOS")
+
+        envio_realizado = True
 
 @app.route("/")
 def index():
